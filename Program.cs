83b6db5aaf9638f8a787d2063bbeb2740fd15710
eduardo-072 <eduardosilvaaa072@gmail.com>
2023-10-03 16:55:50 -0300

    realizei um calculador de parcelas

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("-----FAÇA SEU FINANCIAMENTO-----\n");
+
+Console.WriteLine("Digite o valor total do produto...");
+
+decimal vlTotal = Convert.ToDecimal(Console.ReadLine());
+int qtParcela;
+
+Console.Write("Financiamento em quantas vezes?...");
+qtParcela = Convert.ToInt32 (Console.ReadLine());
+
+decimal vlParcela = vlTotal / qtParcela;
+
+Console.WriteLine($"Você pagará {qtParcela} parcelar de R$ {vlParcela}");
+Console.WriteLine("\nOBRIGADO POR COMPRAR CONOSCO !!!");  
+
